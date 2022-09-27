26a65bed8d07c4f63e7f9c8f890ea0087b202d89
marcelpinotti <marcel.pinotti@gmail.com>
2022-09-27 09:25:38 -0300

    Iniciando o projeto

---
+++ camel-spring/src/main/java/br/com/marcelpinotti/camelspring/CamelSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcelpinotti.camelspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelSpringApplication.class, args);
+	}
+
+}
---
+++ camel-spring/src/test/java/br/com/marcelpinotti/camelspring/CamelSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelpinotti.camelspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
