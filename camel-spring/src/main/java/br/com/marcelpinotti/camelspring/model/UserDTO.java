204d99c2db0b708a9281631d262e448feb37239f
marcelpinotti <marcel.pinotti@gmail.com>
2022-09-27 09:37:03 -0300

    Configurando porta da aplicacao e criando o primeiro DTO

---
+++ camel-spring/src/main/java/br/com/marcelpinotti/camelspring/model/UserDTO.java
@@ -0,0 +1,67 @@
+package br.com.marcelpinotti.camelspring.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class UserDTO {
+
+    @JsonProperty("id")
+    private Long id;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("lastname")
+    private String lastname;
+    @JsonProperty("email")
+    private String email;
+    @JsonProperty("password")
+    private String password;
+
+    public UserDTO(){}
+
+    public UserDTO(Long id, String name, String lastname, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
