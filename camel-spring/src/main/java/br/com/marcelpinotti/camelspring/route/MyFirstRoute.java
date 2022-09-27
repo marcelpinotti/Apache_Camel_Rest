7c0388d1453300be07248d6e9660a1616e0e0e6f
marcelpinotti <marcel.pinotti@gmail.com>
2022-09-27 09:47:58 -0300

    Definindo os rest services - primeiro endpoint usado no controller

--- camel-spring/src/main/java/br/com/marcelpinotti/camelspring/route/MyFirstRoute.java
+++ camel-spring/src/main/java/br/com/marcelpinotti/camelspring/route/MyFirstRoute.java
@@ -2,6 +2,7 @@ package br.com.marcelpinotti.camelspring.route;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.http.MediaType;
 
 public class MyFirstRoute extends RouteBuilder {
     @Override
@@ -13,6 +14,18 @@ public class MyFirstRoute extends RouteBuilder {
                 .host("localhost")
                 .port(8081)
                 .bindingMode(RestBindingMode.auto);
+
+        // Define os rest services (endpoints que serão usados no Controller)
+        rest()
+                .path("/api")
+                .produces(MediaType.APPLICATION_JSON_VALUE)
+                .consumes(MediaType.APPLICATION_JSON_VALUE)
+                // (Métodos e rotas do Controller)
+                .get()
+                .id("rest-camel-users") // id: Identificação da rota (funciona sem, no console aparece routeNº)
+                .path("/camel-users")
+                .to("direct:rest-api-users"); // direct: invocação direta e síncrona de qualquer consumidor
+                // quando um produtor envia uma troca de mensagens
 
     }
 }
