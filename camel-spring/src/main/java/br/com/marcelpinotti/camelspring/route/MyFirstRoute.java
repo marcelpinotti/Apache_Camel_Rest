2955239ca94941ef2617488badcba82123616e42
marcelpinotti <marcel.pinotti@gmail.com>
2022-09-27 09:57:53 -0300

    Definindo de onde os dados serão consumidos no primeiro endpoint da aplicacao

--- camel-spring/src/main/java/br/com/marcelpinotti/camelspring/route/MyFirstRoute.java
+++ camel-spring/src/main/java/br/com/marcelpinotti/camelspring/route/MyFirstRoute.java
@@ -1,6 +1,9 @@
 package br.com.marcelpinotti.camelspring.route;
 
+import br.com.marcelpinotti.camelspring.model.UserDTO;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.http.MediaType;
 
@@ -26,6 +29,18 @@ public class MyFirstRoute extends RouteBuilder {
                 .path("/camel-users")
                 .to("direct:rest-api-users"); // direct: invocação direta e síncrona de qualquer consumidor
                 // quando um produtor envia uma troca de mensagens
+
+        // Define de onde os dados serão consumidos
+        from("direct:rest-api-users")
+                .id("rest-api-users")
+                .log("Inicializando JsonView Api Route")
+                .setHeader(Exchange.HTTP_METHOD, constant("GET")) // Definição do método de solicitação HTTP
+                .to("http://localhost:3000/api/users") // Uri de consumo
+                .unmarshal() // Faz a transformação de mensagens (transforma dados recebidos pela rede)
+                .json(JsonLibrary.Jackson, UserDTO[].class) // Formato de conversão
+                .process(exchange -> exchange.getIn().getBody(UserDTO[].class)); // É um nó capaz de usar, criar ou
+        // modificar uma troca recebida.
+    }
 
     }
 }
